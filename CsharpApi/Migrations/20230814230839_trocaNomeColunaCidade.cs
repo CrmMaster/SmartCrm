6003b11b0f9f7b5b7e8e46104b53f63105a035a9
SmartCrm <nossocrmimob@gmail.com>
2023-08-29 21:06:27 -0300

    29/08 - Jhonatan
    Migrations

---
+++ CsharpApi/Migrations/20230814230839_trocaNomeColunaCidade.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharpapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class trocaNomeColunaCidade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Estado",
+                table: "Usuarios",
+                newName: "Cidade");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cidade",
+                table: "Usuarios",
+                newName: "Estado");
+        }
+    }
+}
